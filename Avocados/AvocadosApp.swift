41e009d262db39ae70169618333ae60c84d0d0b2
Nic Deane <njdeane7@gmail.com>
2021-10-01 12:20:15 +1000

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by Nic Deane on 1/10/21.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
