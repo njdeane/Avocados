f9884f3b8436b0e4c610699091c3da4515f78fbc
Nic Deane <njdeane7@gmail.com>
2021-10-02 10:45:08 +1000

    Add FactsView complete with model and data files.

---
+++ Avocados/Model/FactModel.swift
@@ -0,0 +1,16 @@
+//
+//  FactModel.swift
+//  Avocados
+//
+//  Created by Nic Deane on 2/10/21.
+//
+
+import SwiftUI
+
+struct Fact: Identifiable {
+  var id = UUID()
+  var image: String
+  var content: String
+}
+
+
--- Avocados/View/ContentView.swift
+++ Avocados/View/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
   
   var headers: [Header] = headersData
+  var facts: [Fact] = factsData
   
   var body: some View {
     ScrollView(.vertical, showsIndicators: false) {
@@ -31,6 +32,22 @@ struct ContentView: View {
         
         DishesView()
           .frame(maxWidth: 640) // makes it look good on ipad too by restricting max width
+        
+        // Facts
+        Text("Avocado Facts")
+          .fontWeight(.bold)
+          .modifier(TitleModifier())
+        
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack(alignment: .top, spacing: 60) {
+            ForEach(facts) { item in
+              FactsView(fact: item)
+            }
+          }
+          .padding(.vertical)
+          .padding(.leading, 60)
+          .padding(.trailing, 20)
+        }
         
         
         // Footer
@@ -66,6 +83,6 @@ struct TitleModifier: ViewModifier {
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView(headers: headersData)
+    ContentView(headers: headersData, facts: factsData)
   }
 }
