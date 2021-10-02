c765260b0822da4b62fae8d5e58194e03410af09
Nic Deane <njdeane7@gmail.com>
2021-10-02 15:26:10 +1000

    Create Ripeningview with model and data.

--- Avocados/View/RipeningStagesView.swift
+++ Avocados/View/RipeningStagesView.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 
 struct RipeningStagesView: View {
-    var body: some View {
-        Text("Ripening Stages")
+  
+  var ripeningStages: [Ripening] = ripeningData
+  
+  var body: some View {
+    ScrollView(.horizontal, showsIndicators: false) {
+      VStack{
+        Spacer()
+        HStack(alignment: .center, spacing: 25) {
+          ForEach(ripeningStages) { item in
+            RipeningView(ripening: item)
+          }
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 25)
+        Spacer()
+      }
     }
+    .edgesIgnoringSafeArea(.all)
+  }
 }
 
 struct RipeningStagesView_Previews: PreviewProvider {
-    static var previews: some View {
-        RipeningStagesView()
-    }
+  static var previews: some View {
+    RipeningStagesView(ripeningStages: ripeningData)
+  }
 }
