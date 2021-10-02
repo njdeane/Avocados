a64aa478c7f157cfe258eebc5ea3af3fbdc64c24
Nic Deane <njdeane7@gmail.com>
2021-10-02 13:56:36 +1000

    Add haptics to button press. RecipeDetail view modal presentation after press. Wrapped button to enable the down chevron button to dismiss the view.

--- Avocados/View/RecipeDetailView.swift
+++ Avocados/View/RecipeDetailView.swift
@@ -12,6 +12,7 @@ struct RecipeDetailView: View {
   var recipe: Recipe
   
   @State private var pulsate: Bool = false
+  @Environment(\.presentationMode) var presentationMode
   
   var body: some View {
     ScrollView(.vertical, showsIndicators: false) {
@@ -84,7 +85,7 @@ struct RecipeDetailView: View {
         Spacer()
         VStack {
           Button(action: {
-            // Action
+            self.presentationMode.wrappedValue.dismiss()
           }, label: {
             Image(systemName: "chevron.down.circle.fill")
               .font(.title)
